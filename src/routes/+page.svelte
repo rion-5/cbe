b16ab4c63daebf5d326e03e7c3a3652fa224d4af
rion-5 <sklee@hanyang.ac.kr>
2024-08-07 16:48:35 +0900

    add reduce

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,15 @@
         foreigner: string;
     }
 
+    interface DepartmentCounts {
+        department: string;
+        count: number;
+    }
+
     let facultyList: Faculty[] = [];
-    let tenureTrackFaculty: Faculty[]  = [];
+    let tenureTrackFaculty: Faculty[] = [];
+    let departmentCounts: DepartmentCounts[] = [];
+
     const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
     onMount(async () => {
         const response = await fetch(`${apiBaseUrl}/faculty`);
@@ -30,15 +37,39 @@
             retirementTrack: d.retirementTrack,
             foreigner: d.foreigner,
         }));
-        console.log(facultyList);
-        tenureTrackFaculty = data.filter(faculty => faculty.retirementTrack === "정년트랙");
+        // console.log(facultyList);
+        tenureTrackFaculty = data.filter(
+            (faculty) => faculty.retirementTrack === "정년트랙",
+        );
 
-        console.log(tenureTrackFaculty);
-        
+        departmentCounts = data
+            .filter((faculty) => faculty.retirementTrack === "정년트랙") // 필터링
+            .reduce(
+                (acc, faculty) => {
+                    const existingDepartment = acc.find((item) => item.department === faculty.department);
+                    if (existingDepartment) {
+                        existingDepartment.count++;
+                    } else {
+                        acc.push({ department: faculty.department, count: 1 });
+                    }
+                    return acc;
+                },
+                [] as DepartmentCounts[],
+            );
+
+        console.log(JSON.stringify(departmentCounts, null, 2));
     });
 </script>
-{#each facultyList as item}
-	<li>
-		학과: {item.department}  직급: {item.rank} 정년트랙: {item.retirementTrack}
-	</li>
+
+
+{#each departmentCounts as item}
+    <li>
+        학과: {item.department} 교원수: {item.count}
+    </li>
 {/each}
+
+<!-- {#each facultyList as item}
+    <li>
+        학과: {item.department} 직급: {item.rank} 정년트랙: {item.retirementTrack}
+    </li>
+{/each} -->
