e6d41d8c643a5c2ea8848c03d2e54301d62cdbeb
rion-5 <sklee@hanyang.ac.kr>
2024-08-10 19:35:59 +0900

    edit arcChart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { onMount } from "svelte";
     import DepartmentCountsBarChart from "./DepartmentCountsBarChart.svelte";
+    import RankArcChart from "./RankArcChart.svelte";
     interface Faculty {
         department: string;
         rank: string;
@@ -25,6 +26,7 @@
     let rankEconomics: ProfessorRank[] = [];
     let rankBusiness: ProfessorRank[] = [];
     let rankActuarial: ProfessorRank[] = [];
+    let rankAccounting: ProfessorRank[] = [];
 
     const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
     onMount(async () => {
@@ -69,20 +71,103 @@
 
         //console.log(JSON.stringify(departmentCounts, null, 2));
 
-        rankEconomics = data.filter((faculty) => faculty.(retirementTrack ==="정년트랙"
-        &&department === "경제학부"));
+        rankEconomics = data.filter((faculty) => (faculty.retirementTrack ==="정년트랙"
+         && faculty.department === "경제학부"))
+         .reduce(
+                (acc, faculty) => {
+                    const existingRank = acc.find((item) => item.rank === faculty.rank);
+                    if (existingRank) {
+                        existingRank.count++;
+                    } else {
+                        acc.push({ rank: faculty.rank, count: 1 });
+                    }
+                    return acc;
+                },
+                [] as ProfessorRank[],
+        
+        );
+        // console.log(JSON.stringify(rankEconomics, null, 2));
+        rankBusiness = data.filter((faculty) => (faculty.retirementTrack ==="정년트랙"
+         && faculty.department === "경영학부"))
+         .reduce(
+                (acc, faculty) => {
+                    const existingRank = acc.find((item) => item.rank === faculty.rank);
+                    if (existingRank) {
+                        existingRank.count++;
+                    } else {
+                        acc.push({ rank: faculty.rank, count: 1 });
+                    }
+                    return acc;
+                },
+                [] as ProfessorRank[],
+        
+        );
+        rankActuarial = data.filter((faculty) => (faculty.retirementTrack ==="정년트랙"
+         && faculty.department === "보험계리학과"))
+         .reduce(
+                (acc, faculty) => {
+                    const existingRank = acc.find((item) => item.rank === faculty.rank);
+                    if (existingRank) {
+                        existingRank.count++;
+                    } else {
+                        acc.push({ rank: faculty.rank, count: 1 });
+                    }
+                    return acc;
+                },
+                [] as ProfessorRank[],
+        
+        );
+        rankAccounting = data.filter((faculty) => (faculty.retirementTrack ==="정년트랙"
+         && faculty.department === "회계세무학과"))
+         .reduce(
+                (acc, faculty) => {
+                    const existingRank = acc.find((item) => item.rank === faculty.rank);
+                    if (existingRank) {
+                        existingRank.count++;
+                    } else {
+                        acc.push({ rank: faculty.rank, count: 1 });
+                    }
+                    return acc;
+                },
+                [] as ProfessorRank[],
+        
+        );
 
     });
 </script>
 <div class="container">
 	<h1>경상대 교원현황</h1>
 	<div class="row">
-		<div class="col col-9">
-			<h2>학부(과)별 전임교원수</h2>
+		<div class="col col-3">
+			<h2>전임교원</h2>
 			<div class="responsive-svg-container">
 				<DepartmentCountsBarChart {departmentCounts} />
 			</div>
 		</div>
+        <div class="col col-3">
+			<h2>경제학부</h2>
+			<div class="responsive-svg-container">
+				<RankArcChart {rankEconomics} />
+			</div>
+		</div>
+        <!-- <div class="col col-3">
+			<h2>경영학부</h2>
+			<div class="responsive-svg-container">
+				<RankArcChart {rankBusiness} />
+			</div>
+		</div> -->
+    <!--    <div class="col col-3">
+			<h2>보험계리학과</h2>
+			<div class="responsive-svg-container">
+				<RankArcChart {rankActuarial} />
+			</div>
+		</div>
+        <div class="col col-3">
+			<h2>회계세무학과</h2>
+			<div class="responsive-svg-container">
+				<RankArcChart {rankAccounting} />
+			</div>
+		</div> -->
 	</div>
 </div>
 
--- src/routes/RankArcChart.svelte
+++ src/routes/RankArcChart.svelte
@@ -1,4 +1,96 @@
-<script lang='ts'>
-    import { onMount, afterUpdate } from 'svelte';
-    import * as d3 from 'd3';
-    import './DepartmentCountsBarChart.css';
+<script lang="ts">
+    import { onMount, afterUpdate } from "svelte";
+    import * as d3 from "d3";
+    import "./DepartmentCountsBarChart.css";
+
+    export let rankEconomics: { rank: string; count: number }[];
+    // export let rankBusiness: { rank: string; count: number }[];
+    onMount(() => {
+        drawArc();
+    });
+    afterUpdate(() => {
+        drawArc();
+    });
+    function drawArc() {
+        const pieChartWidth = 300;
+        const pieChartHeight = 300;
+        const svg = d3
+            .select("#arc")
+            .append("svg")
+            .attr("viewBox", [0, 0, pieChartWidth, pieChartHeight]);
+
+        const innerChart = svg
+            .append("g")
+            .attr(
+                "transform",
+                `translate(${pieChartWidth / 2}, ${pieChartHeight / 2})`,
+            );
+
+        const numberOfDays = rankEconomics.length;
+        const numberOfDaysWithPrecipitations = rankEconomics.filter(
+            (d) => d.count > 0,
+        ).length;
+        const percentageDaysWithPrecipitations = Math.round(
+            (numberOfDaysWithPrecipitations / numberOfDays) * 100,
+        );
+
+        const angleDaysWithPrecipitations_deg =
+            (percentageDaysWithPrecipitations * 360) / 100;
+        const angleDaysWithPrecipitations_rad =
+            (angleDaysWithPrecipitations_deg * Math.PI) / 180;
+
+        const arcGenerator = d3
+            .arc()
+            .innerRadius(80)
+            .outerRadius(120)
+            .padAngle(0.02)
+            .cornerRadius(6);
+
+        innerChart
+            .append("path")
+            .attr(
+                "d",
+                arcGenerator({
+                    innerRadius: 80,
+                    outerRadius: 120,
+                    startAngle: 0,
+                    endAngle: angleDaysWithPrecipitations_rad,
+                }),
+            )
+            .attr("fill", "#6EB7C2");
+
+        innerChart
+            .append("path")
+            .attr(
+                "d",
+                arcGenerator({
+                    innerRadius: 80,
+                    outerRadius: 120,
+                    startAngle: angleDaysWithPrecipitations_rad,
+                    endAngle: 2 * Math.PI,
+                }),
+            )
+            .attr("fill", "#DCE2E2");
+
+        const centroid = arcGenerator.centroid({
+            innerRadius: 80,
+            outerRadius: 120,
+            startAngle: 0,
+            endAngle: angleDaysWithPrecipitations_rad,
+        });
+
+        innerChart
+            .append("text")
+            .text((d) =>
+                d3.format(".0%")(percentageDaysWithPrecipitations / 100),
+            )
+            .attr("x", centroid[0])
+            .attr("y", centroid[1])
+            .attr("text-anchor", "middle")
+            .attr("dominant-baseline", "middle")
+            .attr("fill", "white")
+            .style("font-weight", 500);
+    }
+</script>
+
+<div id="arc" style="width: 400px; height: 400px;"></div>
