a1d97e112f2602aa59515398d6179381fa67269a
rion-5 <sklee@hanyang.ac.kr>
2024-09-09 09:21:15 +0900

    change department order

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
+	server: {
+		// host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능하게 설정
+		// port: 3000, // Vite 개발 서버 포트
+		hmr: {
+		//   host: '166.104.191.59', // WebSocket 연결을 위한 호스트
+		  port: 3000
+		}
+	  },
 	plugins: [sveltekit()]
 });
