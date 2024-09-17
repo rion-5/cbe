e296ecead446f3c20070fa1be6df2296e1cd680f
rion5 <rion5@MyPro.local>
2024-09-17 16:16:27 +0900

    add 학기 at query

--- src/routes/api/faculty/+server.ts
+++ src/routes/api/faculty/+server.ts
@@ -10,7 +10,7 @@ export const GET = async () => {
             정년트랙구분 AS retirementtrack,
             외국인여부 AS foreigner
         FROM 교원조회및인사카드출력
-        WHERE 대학 = '경상대학' AND 재직구분 = '재직'
+        WHERE 대학 = '경상대학' AND 재직구분 = '재직' and 학기 = 202402
         ORDER BY array_position(ARRAY['경제학부','경영학부','보험계리학과','회계세무학과'], 학과::text),
                 array_position(ARRAY['전임교원','겸임교수','강사','명예교수'], 직종::text)
     `);
